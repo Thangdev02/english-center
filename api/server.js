d0b50329133a27808a3b55121c2761e8630c4167
Thangdev02 <thangdev02@gmail.com>
2025-10-08 23:17:23 +0700

    test db

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
-// api/server.js
 import jsonServer from "json-server";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
