7d2e306abf1721e908473739622fdc0f050da7ae
Thangdev02 <thangdev02@gmail.com>
2025-10-08 23:26:57 +0700

    final

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = 'https://english-center-19t2.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
